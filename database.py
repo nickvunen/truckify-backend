f05ad4118ce493e785bcbae0fc825b243d50a327
Nick van Unen <nick.van.unen@knmi.nl>
2025-05-09 22:27:16 +0200

    add sample data endpoint

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Date, ForeignKey, Integer, String, create_engine
+from datetime import date, datetime
+
+from sqlalchemy import Boolean, Column, Date, ForeignKey, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -37,3 +39,85 @@ class Booking(Base):
     end_date = Column(Date, nullable=False)
     email = Column(String)
     truck_id = Column(Integer, ForeignKey("trucks.id"), nullable=False)
+    total_price = Column(Integer)
+    paid = Column(Boolean, default=False)
+    confirmed = Column(Boolean, default=False)
+
+
+def fill_trucks(db):
+    # Fill the database with some sample trucks
+    sample_trucks = [
+        Truck(
+            license="ABC123",
+            price_per_day=100,
+            level="Standard",
+            image="https://www.truckcamperadventure.com/wp-content/uploads/2023/07/BCR-Front-3_4-scaled.jpeg",
+        ),
+        Truck(
+            license="XYZ789",
+            price_per_day=150,
+            level="Luxury",
+            image="https://www.trucks.nl/img/956c5ad21d93b4887f80573cdf80d775/motorhome_camper_scania_g410_xt_4x4_fully_equipped_expedition_truck_2023_8114190.jpg??width=2220&height=1664&quality=70",
+        ),
+        Truck(
+            license="LMN456",
+            price_per_day=120,
+            level="Standard",
+            image="https://images.squarespace-cdn.com/content/v1/5a150361c027d861562cd5cd/1707784454333-X67T8T6SNWEEKSSJJVDU/Copy+of+Swissliner+Auto-41.jpg?format=1500w",
+        ),
+    ]
+    db.add_all(sample_trucks)
+    db.commit()
+
+
+def fill_bookings(db):
+    # Fill the database with some sample bookings
+    sample_bookings = [
+        Booking(
+            start_date=date(2025, 1, 1),
+            end_date=date(2025, 1, 10),
+            email="gayguy@hotmail.com",
+            truck_id=1,
+            total_price=500,
+            paid=True,
+            confirmed=True,
+        ),
+        Booking(
+            start_date=date(2025, 4, 1),
+            end_date=date(2025, 5, 10),
+            email="hetroguy@hotmail.com",
+            truck_id=2,
+            total_price=600,
+            paid=True,
+            confirmed=False,
+        ),
+        Booking(
+            start_date=date(2025, 6, 15),
+            end_date=date(2025, 6, 20),
+            email="example@example.com",
+            truck_id=3,
+            total_price=700,
+            paid=False,
+            confirmed=True,
+        ),
+        Booking(
+            start_date=date(2025, 7, 1),
+            end_date=date(2025, 7, 10),
+            email="test@test.com",
+            truck_id=1,
+            total_price=800,
+            paid=True,
+            confirmed=True,
+        ),
+        Booking(
+            start_date=date(2025, 8, 1),
+            end_date=date(2025, 8, 10),
+            email="user@example.com",
+            truck_id=2,
+            total_price=900,
+            paid=False,
+            confirmed=False,
+        ),
+    ]
+    db.add_all(sample_bookings)
+    db.commit()
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from fastapi import Depends, FastAPI
+from fastapi import APIRouter, Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from sqlalchemy import and_, func
 from sqlalchemy.orm import Session
 
-from database import Base, Booking, SessionLocal, StringItem, Truck, engine
+from database import (
+    Base,
+    Booking,
+    SessionLocal,
+    StringItem,
+    Truck,
+    engine,
+    fill_bookings,
+    fill_trucks,
+)
 
 app = FastAPI()
 
@@ -30,15 +40,30 @@ def get_db():
         db.close()
 
 
-##### STRING ENDPOINTS #####
+##### ROUTERS #####
+test_router = APIRouter()
+truck_router = APIRouter()
+booking_router = APIRouter()
+availability_router = APIRouter()
 
 
-@app.get("/string")
+##### TEST ENDPOINTS #####
+
+
+@test_router.get("/string")
 def read_string(db: Session = Depends(get_db)):
     item = db.query(StringItem).first()
     if item:
         return {"result": item.value}
     return {"result": ""}
+
+
+@test_router.post("/fill_database")
+def fill_database(db: Session = Depends(get_db)):
+    # Fill the database with some sample data
+    fill_trucks(db)
+    fill_bookings(db)
+    return {"message": "Database filled with sample data"}
 
 
 ##### TRUCK ENDPOINTS #####
@@ -55,7 +80,7 @@ class TruckResponse(BaseModel):
         from_attributes = True  # Enable compatibility with SQLAlchemy models
 
 
-@app.get("/trucks/{truck_id}")
+@truck_router.get("/{truck_id}")
 def get_truck(truck_id: int, db: Session = Depends(get_db)):
     truck = db.query(Truck).filter(Truck.id == truck_id).first()
     if truck:
@@ -63,13 +88,13 @@ def get_truck(truck_id: int, db: Session = Depends(get_db)):
     return {"error": "Truck not found"}
 
 
-@app.get("/trucks")
+@truck_router.get("/")
 def get_trucks(db: Session = Depends(get_db)):
     trucks = db.query(Truck).all()
     return {"trucks": trucks}
 
 
-@app.post("/trucks", response_model=TruckResponse)
+@truck_router.post("/truck", response_model=TruckResponse)
 def create_truck(truck: TruckResponse, db: Session = Depends(get_db)):
     new_truck = Truck(**truck.model_dump())
     db.add(new_truck)
@@ -78,7 +103,7 @@ def create_truck(truck: TruckResponse, db: Session = Depends(get_db)):
     return new_truck
 
 
-@app.delete("/trucks/{truck_id}")
+@truck_router.delete("/{truck_id}")
 def delete_truck(truck_id: int, db: Session = Depends(get_db)):
     truck = db.query(Truck).filter(Truck.id == truck_id).first()
     if truck:
@@ -90,18 +115,23 @@ def delete_truck(truck_id: int, db: Session = Depends(get_db)):
 
 # ##### BOOKING ENDPOINTS #####
 
+COST_PORTA_POTTI = 60
+COST_CLEANING_SERVICE = 75
+
 
 class BookingResponse(BaseModel):
     start_date: datetime
     end_date: datetime
     email: str
     truck_id: int
+    porta_potti: bool = False
+    cleaning_service: bool = False
 
     class Config:
         from_attributes = True
 
 
-@app.get("/bookings/{booking_id}")
+@booking_router.get("/{booking_id}")
 def get_booking(booking_id: int, db: Session = Depends(get_db)):
     booking = db.query(Booking).filter(Booking.id == booking_id).first()
     if booking:
@@ -109,26 +139,65 @@ def get_booking(booking_id: int, db: Session = Depends(get_db)):
     return {"error": "Booking not found"}
 
 
-@app.get("/bookings")
+@booking_router.get("/")
 def get_bookings(db: Session = Depends(get_db)):
     bookings = db.query(Booking).all()
     return {"bookings": bookings}
 
 
-@app.post("/bookings")
+@booking_router.post("/booking")
 def create_booking(booking: BookingResponse, db: Session = Depends(get_db)):
     new_booking = Booking(**booking.model_dump())
+
+    # Check that the start_date and end_date are at least three days apart
+    if (booking.end_date - booking.start_date).days < 3:
+        return {"error": "Booking must be at least 3 days long"}
+
+    price = db.query(Truck.price_per_day).filter(Truck.id == booking.truck_id).first()
+    if price:
+        new_booking.total_price = (
+            price[0] * (booking.end_date - booking.start_date).days
+        )
+        if booking.porta_potti:
+            new_booking.total_price += COST_PORTA_POTTI
+        if booking.cleaning_service:
+            new_booking.total_price += COST_CLEANING_SERVICE
+    else:
+        return {"error": "Truck not found"}
+
     db.add(new_booking)
     db.commit()
     db.refresh(new_booking)
     return {"booking": new_booking}
 
 
-@app.delete("/bookings/{booking_id}")
-def delete_booking(booking_id: int, db: Session = Depends(get_db)):
+@booking_router.put("/paid/{booking_id}")
+def put_booking_paid(booking_id: int, paid: bool, db: Session = Depends(get_db)):
     booking = db.query(Booking).filter(Booking.id == booking_id).first()
     if booking:
-        db.delete(booking)
+        booking.paid = paid
+        db.commit()
+        return {"message": "Booking updated"}
+    return {"error": "Booking not found"}
+
+
+@booking_router.put("/confirm/{booking_id}")
+def put_booking_confirm(
+    booking_id: int, confirmed: bool, db: Session = Depends(get_db)
+):
+    booking = db.query(Booking).filter(Booking.id == booking_id).first()
+    if booking:
+        booking.confirmed = confirmed
+        db.commit()
+        return {"message": "Booking updated"}
+    return {"error": "Booking not found"}
+
+
+@booking_router.delete("/{booking_id}")
+def delete_booking(booking_id: int, confirmed: bool, db: Session = Depends(get_db)):
+    booking = db.query(Booking).filter(Booking.id == booking_id).first()
+    if booking:
+        booking.confirmed = confirmed
         db.commit()
         return {"message": "Booking deleted"}
     return {"error": "Booking not found"}
@@ -142,7 +211,7 @@ class TruckAvailabilityResponse(BaseModel):
     end: datetime
 
 
-@app.post("/trucks_available")
+@availability_router.post("/trucks_available")
 def post_available_trucks(
     truck_availability: TruckAvailabilityResponse, db: Session = Depends(get_db)
 ):
@@ -157,4 +226,26 @@ def post_available_trucks(
         .all()
     )
 
-    return {"available_trucks": available_trucks}
+    # Query to find all trucks that are booked but are near the end of their booking period
+    buffer_period = func.interval("3 day")
+    proposed_trucks = (
+        db.query(Truck)
+        .outerjoin(Booking)
+        .filter(
+            and_(
+                (Booking.start_date >= start_date - buffer_period)
+                & (Booking.start_date < end_date),
+                (Booking.end_date <= end_date + buffer_period)
+                & (Booking.end_date > start_date),
+            )
+        )
+        .all()
+    )
+
+    return {"available_trucks": available_trucks, "proposed_trucks": proposed_trucks}
+
+
+app.include_router(test_router, prefix="/test", tags=["test"])
+app.include_router(truck_router, prefix="/trucks", tags=["trucks"])
+app.include_router(booking_router, prefix="/bookings", tags=["bookings"])
+app.include_router(availability_router, prefix="/availability", tags=["availability"])
